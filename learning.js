61bb045e432539e649927297668083d11390f0bf
Đỗ Kim Nhường <127850790+KimNhuong@users.noreply.github.com>
2025-06-19 17:44:03 +0700

    Rock, Paper, Scissors game using pure JS,HTML and CSS

---
+++ learning.js
@@ -0,0 +1,9 @@
+console.log(`Learning fuck through building a Webpage`);
+Document.getElementById("TrueHeading").textContent = `hello`;
+window.alert(`this is an alert!`);
+
+
+let x; 
+x = 4; 
+
+console.log(x + `4 = ` + x + 4);
